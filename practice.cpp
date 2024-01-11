e8eae07976fb29d3bc36671487e907fd6e17f653
gaochen <125664573+gaochen61@users.noreply.github.com>
2024-01-11 22:04:45 +0800

    Add files via upload

---
+++ practice.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+int add(int n);
+int main()
+{
+	int orin_sum = add(21 * 20);
+	int ans = orin_sum - (20 * (add(21) - 1))- (21 * (add(20) - 1));
+	printf("%d\n", ans);
+	for (int i = 4; i < 21; i++)
+	{
+		ans -= (add(i) - 1)*2;
+	}
+	printf("%d", ans);
+}
+
+int add(int n)
+{
+	int i, sum = 0;
+	for (i = 1; i < n; i++)
+	{
+		sum += i;
+	}
+	return sum;
+}
